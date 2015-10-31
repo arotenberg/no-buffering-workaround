007a1dd8003f9e6a7579bbaddd00a9c9be8320f8
Aaron Rotenberg <arotenberg@users.noreply.github.com>
2015-10-31 16:11:15 -0400

    Fixing broken Haddock markup.

--- src/System/IO/NoBufferingWorkaround.hs
+++ src/System/IO/NoBufferingWorkaround.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE CPP #-}
 
 {- |
-This package exists as a workaround for GHC bug #2189, "@hSetBuffering stdin
-NoBuffering@ doesn't work on Windows". It provides functionality for reading
+This package exists as a workaround for GHC bug #2189, \"@hSetBuffering stdin
+NoBuffering@ doesn't work on Windows\". It provides functionality for reading
 from standard input without buffering, in a way that works under GHC on
 Windows as well as other configurations. This is useful for key-driven console
 applications such as roguelikes.
